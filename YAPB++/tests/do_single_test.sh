7ec024511d29d575d72c911f9cfbb0799c72adea
Chris Jefferson <caj21@st-andrews.ac.uk>
2016-07-11 11:35:42 -0400

    Nearly?

--- YAPB++/tests/do_single_test.sh
+++ YAPB++/tests/do_single_test.sh
@@ -2,4 +2,4 @@
 . ../../ferret.vars
 cat ../../ferret.vars
 # echo $CXX $1  -I ../source -I ../../gap_cpp_headers -DYAPB_NO_GAP $DEBUGFLAGS -g -DDEBUG_LEVEL=2 -Wall -Wextra -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
-eval $CXX $1 -I ../source -I ../../gap_cpp_headers -DYAPB_NO_GAP $DEBUGFLAGS -g -DDEBUG_LEVEL=2 -Wall -Wextra -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
+eval $CXX $1 -std=c++11 -I ../source -I ../../gap_cpp_headers -DYAPB_NO_GAP $DEBUGFLAGS -g -DDEBUG_LEVEL=2 -Wall -Wextra -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
