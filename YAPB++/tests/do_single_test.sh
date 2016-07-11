fc30e7bfabf6f4f8fcea668cae68c74d4ff0e87a
Chris Jefferson <caj21@st-andrews.ac.uk>
2016-07-11 11:49:42 -0400

    Updatey

--- YAPB++/tests/do_single_test.sh
+++ YAPB++/tests/do_single_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 . ../../ferret.vars
 cat ../../ferret.vars
-# echo $CXX $1  -I ../source -I ../../gap_cpp_headers -DYAPB_NO_GAP $DEBUGFLAGS -g -DDEBUG_LEVEL=2 -Wall -Wextra -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
-eval $CXX $1 -std=c++11 -I ../source -I ../../gap_cpp_headers -DYAPB_NO_GAP $DEBUGFLAGS -g -DDEBUG_LEVEL=2 -Wall -Wextra -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
+echo ${CXX:-c++} $1 -std=c++11 -I ../source -I ../../gap_cpp_headers -DYAPB_NO_GAP $DEBUGFLAGS -g -DDEBUG_LEVEL=2 -Wall -Wextra -o $1.exe 
+${CXX:-c++} $1 -std=c++11 -I ../source -I ../../gap_cpp_headers -DYAPB_NO_GAP $DEBUGFLAGS -g -DDEBUG_LEVEL=2 -Wall -Wextra -o $1.exe && $VALGRIND ./$1.exe && rm -r $1.exe*
